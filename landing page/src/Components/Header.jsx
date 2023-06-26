29a2de02e7018e3e4cd2fe183883a27254b3cf38
harish220599 <97904649+harish220599@users.noreply.github.com>
2023-06-26 22:28:11 +0530

    added comments

--- landing page/src/Components/Header.jsx
+++ landing page/src/Components/Header.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-
+import React,{ useState} from 'react'
+import { HiBars3BottomRight,HiPause } from "react-icons/hi2";
 function Header() {
+    const[toggle,setToggle]=useState(true)
     const menuList=[
         {
             id:1,
@@ -27,7 +28,7 @@ function Header() {
             <h2 className="text-white font-bold">Harine
             <span className=" text-customRed"> G.S</span></h2>
         </div>
-        <div className="flex gap-7 "> 
+        <div className="hidden md:flex gap-4 "> 
             {menuList.map((item)=>(
                 <div>
                     <h2 className="text-white hover:border-[1px] border-red-500 rounded-fulltext-[15px]">
@@ -38,6 +39,11 @@ function Header() {
             <h2 className='text-white
             hover:border-[1px] border-red-500 rounded-full
             text-[15px] px-3 py-1 cursor-pointer hover:bg-gradient-to-r from-red-500 to-red-800'>Hire Me</h2>
+        </div>
+        <div className='md:hidden'>
+            {toggle?<HiBars3BottomRight onClick={()=>setToggle(!toggle)} className="text-white text-[22px] cursor-pointer"/>
+            :<HiPause onClick={()=>setToggle(!toggle)} className="text-white text-[22px]"/>}
+            
         </div>
     </div>
 )
